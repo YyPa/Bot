0b849fef9edb6922a7086c96a7fdb092da75b5d8
YyPa <40070476+YyPa@users.noreply.github.com>
2018-09-11 18:10:48 +0300

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -1,36 +1,23 @@
 const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
+const client = new Discord.Client();
 
-var prefix = ayarlar.prefix;
-
-exports.run = (client, message, params) => {
-
-  if (!params[0]) {
-    const commandNames = Array.from(client.commands.keys());
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.author.sendCode('asciidoc', `= Komut Listesi =\n\n[Komut hakkında bilgi için ${ayarlar.prefix}yardım <komut adı>]\n\n${client.commands.map(c => `${ayarlar.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
+exports.run = (client, message) => {
   if (message.channel.type !== 'dm') {
-    const ozelmesajkontrol = new Discord.RichEmbed()
-    message.channel.sendEmbed(ozelmesajkontrol) }
-  } else {
-    let command = params[0];
-    if (client.commands.has(command)) {
-      command = client.commands.get(command);
-      message.author.sendCode('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
-    }
-  }
-
+	const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setDescription('Hey Sen Sunucuma Gelirmisin : Davet Linki : https://discord.gg/c9KsCSJ');
+    message.author.sendEmbed(ozelmesajkontrol) }
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['h', 'halp', 'help', 'y'],
+  aliases: [],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'yardım',
-  description: 'Tüm komutları gösterir.',
-  usage: 'yardım [komut]'
+  name: 'sa',
+  description: 'Davet Gönderir',
+  usage: 'sa'
 };
